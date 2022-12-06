04561fbf5b1ce81bc3c8d7a962e8316798c79e21
SonaliTCQ <119936733+SonaliTCQ@users.noreply.github.com>
2022-12-06 17:48:44 +0530

    Create misaligned.hpp

---
+++ misaligned.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+char *printfirstmajorcolor(int, int, int);
+char *printfirstmajorcolor(int, int, int);
+char *printsecondmajorcolor(int, int, int);
+char *printthirdmajorcolor(int, int, int);
+char *printfourthmajorcolor(int, int, int);
+char *printfifthmajorcolor(int, int, int);
+void test_minor_color();
