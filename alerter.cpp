e7c95d382aa14bbd37e7cc48f0e29c76316ad8a5
SonaliTCQ <119936733+SonaliTCQ@users.noreply.github.com>
2022-12-06 17:50:02 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
 #include <assert.h>
+#include "alerter.hpp"
+#include "alerter_stub.cpp"
+#include "alerter_test.hpp"
 
-int alertFailureCount = 0;
+#define CEL_OKTHR 200
+#define CEL_NOKTHR 500
 
-int networkAlertStub(float celcius) {
-    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
-}
+#define SWENV 0
+    // if production then set to 1, if testing then set to 0
 
 void alertInCelcius(float farenheit) {
+    
+    int alertFailureCount = 0;
     float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
-    if (returnCode != 200) {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
-    }
+    
+        if(!SWENV){             //testing environment
+            int returnCode = networkAlertStub(celcius);
+            if (returnCode != CEL_OKTHR) {
+                // non-ok response is not an error! Issues happen in life!
+                // let us keep a count of failures to report
+                // However, this code doesn't count failures!
+                // Add a test below to catch this bug. Alter the stub above, if needed.
+                alertFailureCount += 0;
+                }
+             alerttest(farenheit, alertFailureCount);
+            }
 }
 
 int main() {
+    alertInCelcius(392);
     alertInCelcius(400.5);
     alertInCelcius(303.6);
-    std::cout << alertFailureCount << " alerts failed.\n";
+   
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
