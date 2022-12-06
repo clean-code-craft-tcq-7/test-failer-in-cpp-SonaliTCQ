08c1ec43c8f9a54a1df33ca0965a81989804a0b7
SonaliTCQ <119936733+SonaliTCQ@users.noreply.github.com>
2022-12-06 19:19:32 +0530

    Update printminorcolors.cpp

--- printminorcolors.cpp
+++ printminorcolors.cpp
@@ -5,7 +5,7 @@ char *printfirstmajorcolor(int i, int first, int n){
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 	
     std::cout << i * n + first << " " " | " << majorColor[i] << " " " | " << minorColor[first] << "\n";
-	const char* check = minorColor[i];
+	const char* check = minorColor[first];
 	return (char *)check;
 }
 
@@ -14,7 +14,7 @@ char *printsecondmajorcolor(int i, int second, int n){
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     
     std::cout << i * n + second << " " " | " << majorColor[i] << " " " " " " " | " << minorColor[second] << "\n";
-	const char* check = minorColor[i];
+	const char* check = minorColor[second];
 	return (char *)check;
 }
 
@@ -23,7 +23,7 @@ char *printthirdmajorcolor(int i, int third, int n){
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     
     std::cout << i * n + third << " | " << majorColor[i] << " " " | " << minorColor[third] << "\n";
-    const char* check = minorColor[i];
+    const char* check = minorColor[third];
 	return (char *)check;
 }
 
@@ -32,7 +32,7 @@ char *printfourthmajorcolor(int i, int fourth, int n){
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     
     std::cout << i * n + fourth << " | " << majorColor[i] << " | " << minorColor[fourth] << "\n";
-    const char* check = minorColor[i];
+    const char* check = minorColor[fourth];
 	return (char *)check;
 }
 
@@ -41,6 +41,6 @@ char *printfifthmajorcolor(int i, int fifth, int n){
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     
     std::cout << i * n + fifth << " | " << majorColor[i] <<  " | " << minorColor[fifth] << "\n";
-    const char* check = minorColor[i];
+    const char* check = minorColor[fifth];
 	return (char *)check;
 }
